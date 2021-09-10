9d43d0ed2306aac431d4ac4dc7d3ac81b18784f4
SamuelKelechi <kcfreshie@ymail.com>
2021-09-10 23:39:02 +0100

    Second push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <InputHolder>
+        <Image>Image</Image>
+        <FormInput>
+          <input type="file"/>
+          <input />
+          <input />
+        </FormInput>
+      </InputHolder>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+width: 100vw;
+height: 100%;
+color: white;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const InputHolder = styled.div`
+height: 400px;
+width: 600px;
+border: 1px solid black;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 20px;
+padding: 15px;
+`
+
+const Image = styled.div`
+background-color: red;
+height: 300px;
+width: 300px;
+border-radius: 50%;
+margin: 7px;
+object-fit: cover;
+display: flex;
+
+`
+
+const FormInput = styled.div`
+display: flex;
+flex-direction: column;
+`
