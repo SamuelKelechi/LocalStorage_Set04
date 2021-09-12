30c47c57a499e4bf14dfc71ab6f3d99727dae01e
SamuelKelechi <kcfreshie@ymail.com>
2021-09-12 11:45:05 +0100

    Done with UI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-distracting-elements */
 import styled from 'styled-components';
 
 
@@ -9,17 +10,31 @@ function App() {
           <Image />
           <FormInput>
             <input type="file"/>
-            <input />
-            <input />
+            <input placeholder="Name"/>
+            <input placeholder="Address"/>
+            <input placeholder="Phone Number"/>
+            <textarea rows="5" cols="50" placeholder="Description"/>
           </FormInput>
         </InputHolder>
         <button>
           Submit
         </button>
       </PostCard>
+
+      <Info><marquee>Displayed Cards Here</marquee></Info>
+
       <CardWrapper>
         <Card>
-          Hello World
+          <Top>
+            <div>Icon</div>
+            <div>Del</div>
+          </Top>
+          <Border></Border>
+          <ProfImage />
+          <Name>Name</Name>
+          <Address>Address</Address>
+          <PhoneNum>Phone</PhoneNum>
+          <Descr>Description</Descr>
         </Card>
       </CardWrapper>
     </Container>
@@ -29,22 +44,25 @@ function App() {
 export default App;
 
 const Container = styled.div`
-width: 100vw;
-height: 100%;
+width: 100%;
+height: auto;
 display: flex;
 flex-direction: column;
 align-items: center;
+background-color: #26262A;
+color: white;
 `
 
 const PostCard = styled.div`
 width: 600px;
-border: 1px solid black;
 margin-top: 20px;
 display: flex;
 flex-direction: column;
 align-items:center;
+border: 1px solid whitesmoke;
+background-color: #38393A;
 
-  & button {
+   button {
     height: 40px;
     width: 100px;
     background: green;
@@ -61,12 +79,13 @@ align-items:center;
 `
 
 const InputHolder = styled.div`
-width: 600px;
+width: 590px;
 display: flex;
 justify-content: center;
 align-items: center;
 padding: 15px;
 border-radius: 8px;
+
 
 @media screen and (max-width: 650px) {
   flex-direction: column;
@@ -84,28 +103,108 @@ object-fit: cover;
 display: flex;
 justify-content: center;
 align-items: center;
+border: 2px solid white;
+
+@media screen and (max-width: 650px) {
+  width: 90%
+}
 `
 
 const FormInput = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: space-around;
-height: 150px;
+height: 250px;
 width: 250px;
 
-  & input {
-    height: 30px
+   input {
+    height: 30px;
+    border-radius: 5px;
+    padding-left: 8px;
   }
+
+  textarea {
+    resize: none;
+    border-radius: 5px;
+    padding-left: 8px;
+`
+
+const Info = styled.div`
+display: flex;
+justify-content: center;
+font-size: 25px;
+width: 250px;
+margin-top: 15px;
+font-weight: bolder;
+margin-bottom: 15px;
+border-top: 1px solid whitesmoke;
+border-bottom: 1px solid whitesmoke;
+color: #FFEB9E;
 `
 
 const CardWrapper = styled.div`
   width: 98%;
   flex-wrap: wrap;
+  display: flex;
+  justify-content: center;
 `
 
 const Card = styled.div`
   height: 350px;
-  width: 320px;
+  width: 280px;
+  display: flex;
   flex-direction: column;
-  
+  align-items: center;
+  background-color: #2A2B2E;
+  margin: 10px;
+  border-radius: 8px;
+  border: 1px solid white;
+  box-shadow: 5px 3px 7px #ccc;
+
+  @media screen and (max-width: 330px) {
+    width: 90%
+  }
+`
+
+const Top = styled.div`
+width: 97%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+height: 50px;
+
+div{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid white;
+  border-radius: 50%;
+  height: 35px;
+  width: 35px;
+}
+`
+const Border = styled.div`
+height: 50px;
+border-bottom: 1px solid white;
+width: 100%;
+`
+const ProfImage = styled.img`
+display: flex;
+justify-content: center;
+align-items: center;
+object-fit: cover;
+height: 150px;
+width: 150px;
+border-radius: 50%;
+border: 2px solid white;
+margin-top: -70px;
+background-color: whitesmoke
+`
+const Name = styled.div`
+`
+const Address = styled.div`
+`
+const PhoneNum = styled.div`
+`
+const Descr = styled.div`
 `
